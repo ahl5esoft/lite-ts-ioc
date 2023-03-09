50b67b69c3d91fae6f8b4716039af47a7d69c6cb
linyimin <ahl5esoft@gmail.com>
2023-03-09 16:42:15 +0800

    docs: v1.0.0

--- lite-ts-ioc.d.ts
+++ lite-ts-ioc.d.ts
@@ -1,2 +1,7 @@
 declare function get<T>(typer: any): T;
 declare function set(typer: any, value: any): void;
+declare const ioc: {
+    get: typeof get;
+    set: typeof set;
+};
+{};
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const instance = {};
  * 获取实例
  * @param typer 键
  */
-export function get<T>(typer: any) {
+function get<T>(typer: any) {
     const key = getKey(typer);
     if (key)
         return instance[key] as T;
@@ -13,23 +13,21 @@ export function get<T>(typer: any) {
 }
 
 /**
- * 设置容器内容
- * @param typer 键
- * @param value 值
- */
-export function set(typer: any, value: any) {
-    const key = getKey(typer);
-    instance[key] = value;
-}
-
-/**
  * 获取键
  * @param typer 键
  */
 function getKey(typer: any) {
-    if (typeof typer == 'string')
-        return typer;
-    else {
-        return typer.ctor ?? typer.name;
-    }
+    return typeof typer == 'string' ? typer : typer.ctor ?? typer.name;
 }
+
+/**
+ * 设置容器内容
+ * @param typer 键
+ * @param value 值
+ */
+function set(typer: any, value: any) {
+    const key = getKey(typer);
+    instance[key] = value;
+}
+
+export const ioc = { get, set };
